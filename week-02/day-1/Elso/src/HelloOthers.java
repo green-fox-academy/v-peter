40b6654add58345d58ddb3fa82f699dbf56de031
Peter Vadasz <vadasz.peter@freemail.hu>
2018-10-29 13:04:14 +0100

    3-4

---
+++ week-02/day-1/Elso/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello István!");
+        System.out.println("Hello Géza!");
+//        int n = 5;
+//        double myNumber = 3.0 ;
+//        double sum = myNumber + n;
+//        System.out.println(sum);
+
+      //  System.out.println(5. / 2);
+
+}
+}
