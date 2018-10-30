fff87711a8517ce09a1fba45bf7fe5a6f89fb98e
Peter Vadasz <vadasz.peter@freemail.hu>
2018-10-30 17:38:46 +0100

    unique modified

--- week-02/day-2/Unique.java
+++ week-02/day-2/Unique.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Unique {
     public static void main(String[] args) {
@@ -6,19 +7,40 @@ public class Unique {
         //  Returns a list of numbers where every number in the list occurs only once
 
         //  Example
-        //System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+        System.out.println(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
         //  should print: `[1, 11, 34, 52, 61]`
     }
 
-//    static String unique(int[] n){
-//        int[] o;
-//
-//        for (int i = 0; i < n.length; i++) {
-//
-//        }
-//
-//
-//        return o.toString();
-//    }
+    static String unique(int[] n) {
+    //AZ UJ TOMB ELEMSZAMA
+
+        //lemasolom az eredeti tombot
+        int[] o = n.clone();
+        // bevezetek egy szamlalot az ismetlodesekre
+        int count = 0;
+        //a tomb minden elemét vizsgalom minden elemével (tombhossz x tombhossz)
+        for (int i = 0; i < n.length; i++) {
+            for (int j = 0; j < n.length; j++) {
+                if (o[j] == n[i]) {
+                    count++;
+                    //ha megtalalja az elso ismetlodest, rogton kilep a kovetkezo beture
+                    if (count == 2) break;
+                }
+            }
+        }
+        // a letrehozando uj tomb hosszat hatarozom meg a duplikaciok szamabol és az eredeti tomb hosszabol
+        int diff = (count - n.length) / 2;
+        //letrehozom az uj p tombot
+        int[] p = new int[n.length - diff];
+
+
+
+
+        System.out.println(diff);
+        String f = "nemjo";
+        return f;
+//        int[] noDuplicates = IntStream.of(n).distinct().toArray();
+//        return Arrays.toString(noDuplicates);
+    }
 
 }
