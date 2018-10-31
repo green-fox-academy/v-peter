4079c6dc305502773ef0eddcdc334e3dd8606082
Peter Vadasz <vadasz.peter@freemail.hu>
2018-10-31 14:32:04 +0100

    unique works

--- week-02/day-2/Unique.java
+++ week-02/day-2/Unique.java
@@ -1,46 +1,67 @@
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Unique {
+
     public static void main(String[] args) {
         //  Create a function that takes a list of numbers as a parameter
         //  Returns a list of numbers where every number in the list occurs only once
-
         //  Example
-        System.out.println(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many numbers?");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Type the " + i + ". number, please");
+            int o = sc.nextInt();
+            array[i] = o;
+        }
+        //int[] test = {1, 2, 3, 4, 5, 2, 5, 9, 8, 7};
+        System.out.println(unique(array));
+        //System.out.println(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
         //  should print: `[1, 11, 34, 52, 61]`
     }
 
-    static String unique(int[] n) {
-    //AZ UJ TOMB ELEMSZAMA
+    static String unique(int[] array) {
 
-        //lemasolom az eredeti tombot
-        int[] o = n.clone();
+        //AZ UJ TOMB ELEMSZAMANAK MEGALLAPITASA
         // bevezetek egy szamlalot az ismetlodesekre
         int count = 0;
-        //a tomb minden elemét vizsgalom minden elemével (tombhossz x tombhossz)
-        for (int i = 0; i < n.length; i++) {
-            for (int j = 0; j < n.length; j++) {
-                if (o[j] == n[i]) {
-                    count++;
-                    //ha megtalalja az elso ismetlodest, rogton kilep a kovetkezo beture
-                    if (count == 2) break;
+        //a tomb minden elemét vizsgalom minden elemével
+        for (int i = 0; i < array.length; i++) {
+            int iteration = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == array[i] && i < j) {
+                    iteration++;
                 }
             }
+            //ha legalább egyszer ismétlődik:
+            if (iteration > 0) count++;
         }
-        // a letrehozando uj tomb hosszat hatarozom meg a duplikaciok szamabol és az eredeti tomb hosszabol
-        int diff = (count - n.length) / 2;
+        // az uj tomb hosszat hatarozom meg a duplikaciok szamabol és az eredeti tomb hosszabol
+        int newLength = array.length - count;
         //letrehozom az uj p tombot
-        int[] p = new int[n.length - diff];
+        int[] p = new int[newLength];
 
-
-
-
-        System.out.println(diff);
-        String f = "nemjo";
-        return f;
-//        int[] noDuplicates = IntStream.of(n).distinct().toArray();
-//        return Arrays.toString(noDuplicates);
+        //AZ UJ TOMB FELTOLTESE
+        //az uj tomb elso eleme
+        int k = 0;
+        //léptetem az eredeti tömböt
+        for (int i = 0; i < array.length; i++) {
+            // egyedi-e a szám ? számolom a duplikációkat
+            int newCount = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == array[i] && i < j)
+                    newCount++;
+            }
+            if (newCount == 0) {
+                //beirom a tombbe
+                p[k] = array[i];
+                //léptetem az uj tömbindexet
+                k++;
+            }
+        }
+        return Arrays.toString(p);
     }
-
 }
